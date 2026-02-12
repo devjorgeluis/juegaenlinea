12661f47f741f33100bbf57b484ac91ae2edd289
devjorgeluis <jorgencnc0608@gmail.com>
2026-02-12 18:57:46 +0800

    desktop footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,6 +1,8 @@
 import { useOutletContext, useNavigate } from "react-router-dom";
-// import ImgCasino from "/src/assets/img/footer-888cas.png";
-// import ImgSports from "/src/assets/img/footer-888spo.png";
+import ImgLogo from "/src/assets/img/logo.png";
+import ImgJelguru from "/src/assets/svg/jelguru.svg";
+import ImgGcbgreen from "/src/assets/img/gcb-green.png";
+import Img18 from "/src/assets/img/age-more-18.png";
 
 const Footer = () => {
     const navigate = useNavigate();
@@ -8,129 +10,64 @@ const Footer = () => {
 
     return (
         <footer>
-            <div className="footer-component-v2">
-                <div className="footer-container">
-                    <div className="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
-                        <div className="experiencefragment aem-GridColumn aem-GridColumn--default--12">
-                            <div id="experiencefragment-f74107b615" className="cmp-experiencefragment cmp-experiencefragment--offer-strip-es-footer">
-                                <div className="xf-content-height">
-                                    <div className="root container responsivegrid">
-                                        <div className="root container responsivegrid">
-                                            <div id="container-89a2efe923" className="cmp-container">
-                                                <div className="offer-banner">
-                                                    <div className="section section-cta">
-                                                        <div className="footer-banner-text">
-                                                            <span className="footer-banner-second-title">¿PREPARADO PARA CREAR TU CUENTA?</span>
-                                                        </div>
-                                                        <div className="footer-banner-button">
-                                                            <div>
-                                                                <div className="cta-item">
-                                                                    <div className="cta-template cta-secondary ">
-                                                                        <a href="javascript:void(0)"><span className="label ">Jugar</span></a></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            <div className="container">
+                <div className="footer-head">
+                    <div className="footer-head-item">
+                        <div className="footer-head-item-body">
+                            {
+                                isSlotsOnly === "false" ? 
+                                <>
+                                    <a onClick={() => navigate("/home")} className="a-link">Inicio</a>
+                                    <a onClick={() => navigate("/casino")} className="a-link">Casino</a>
+                                    <a onClick={() => navigate("/live-casino")} className="a-link">Casino en vivo</a>
+                                    <a onClick={() => navigate("/sports")} className="a-link">Deportes</a>
+                                    <a onClick={() => navigate("/live-sports")} className="a-link">Deportes en vivo</a>
+                                </> : 
+                                <a onClick={() => navigate("/casino")} className="a-link">Casino</a>
+                            }
                         </div>
                     </div>
-                    <div className="section section-top">
-                        <div className="footer-top">
-                            <div className="footer-top-component">
-                                <div className="section section-link">
-                                    <div className="footer-top-links">
-                                        <div className="footer-top-links-component default">
-                                            <div className="accordion-heading">
-                                                <span className="toggle-icon">
-                                                    <a><span>Juegos de casino</span></a>
-                                                </span>
-                                            </div>
-                                            <div className="accordion-content">
-                                                {
-                                                    isSlotsOnly === "false" ? 
-                                                    <ul className="link-list">
-                                                        <li className="link-item" onClick={() => navigate("/home")}>
-                                                            <a>
-                                                                <span>Inicio</span>
-                                                            </a>
-                                                        </li>
-                                                        <li className="link-item" onClick={() => navigate("/casino")}>
-                                                            <a>
-                                                                <span>Casino</span>
-                                                            </a>
-                                                        </li>
-                                                        <li className="link-item" onClick={() => navigate("/live-casino")}>
-                                                            <a>
-                                                                <span>Casino en vivo</span>
-                                                            </a>
-                                                        </li>
-                                                        <li className="link-item" onClick={() => navigate("/sports")}>
-                                                            <a>
-                                                                <span>Deportes</span>
-                                                            </a>
-                                                        </li>
-                                                        <li className="link-item" onClick={() => navigate("/live-sports")}>
-                                                            <a>
-                                                                <span>Deportes en vivo</span>
-                                                            </a>
-                                                        </li>
-                                                    </ul> : 
-                                                    <ul className="link-list">
-                                                        <li className="link-item" onClick={() => navigate("/casino")}>
-                                                            <a>
-                                                                <span>Casino</span>
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="footer-top-links">
-                                        <div className="footer-top-links-component icons">
-                                            <div className="accordion-heading">
-                                                <span className="toggle-icon">
-                                                    <a><span>2 productos, 1 cuenta</span></a>
-                                                </span>
-                                            </div>
-                                            {/* <div className="accordion-content">
-                                                <ul className="link-list">
-                                                    <li className="link-item" onClick={() => navigate("/casino")}>
-                                                        <a>
-                                                            <img src={ImgCasino} />
-                                                        </a>
-                                                    </li>
-                                                    {
-                                                        isSlotsOnly === "false" && 
-                                                        <li className="link-item" onClick={() => navigate("/sports")}>
-                                                            <a>
-                                                                <img src={ImgSports} />
-                                                            </a>
-                                                        </li>
-                                                    }
-                                                </ul>
-                                            </div> */}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div className="footer-head-item">
+                        <div className="footer-head-item-title">Únete a nosotros</div>
+                        <div className="footer-head-item-body">
+                            <a href="https://www.instagram.com/juegaenlinea/" target="_blank" className="a-social">
+                                <span className="a-span"><i className="fa-brands fa-instagram"></i></span>
+                            </a>
+                            <a href="https://www.facebook.com/juegaenlineacom/" target="_blank" className="a-social">
+                                <span className="a-span"><i className="fa-brands fa-facebook"></i></span>
+                            </a>
                         </div>
                     </div>
-                    <div className="section section-disclaimer">
-                        <div id="footerLicense">
-                            <p><span className="font-size-16">PREVENCIÓN ADICCIÓN AL JUEGO, LLAMA: 900 533 788</span></p>
-                            <p>Copyright © 2026. 888 Online Games España S.A., Licencias nº 138-11/GA/A75057273/SGR, 68-11/GO/N0440805J/SGR, 76-11/ADC/N0440805J/SGR, 70-11/BNG/N0440805J/SGR, 73-11/BLJ/N0440805J/SGR, 71-11/POQ/N0440805J7SGR, MAZ/2014/008, una filial de Evoke Holding Plc.</p>
-                            <p>888 Online Games España S.A. es una sociedad inscrita en Ceuta; Domicilio social: Calle Millán Astray nº1, 51001, Ceuta, España.</p>
-                            <p>El representante en España de 888 es Cuatrecasas con domicilio social en Calle Almagro 9, 28010 – Madrid</p>
-                            <p>La prestación y operación de servicios de juego remoto en España por parte de 888 Online Games España S.A. ha sido autorizada por la Dirección General de Ordenación del Juego.</p>
-                            Para más información, por favor lea nuestros 
-                            <a title="Términos y Condiciones" rel="noopener" className="addsCut">Términos y Condiciones</a>
-                            <a title="Política de Privacidad." rel="noopener" className="addsCut">Política de Privacidad.</a>
+                </div>
+                
+                <div className="footer-finish">
+                    <div className="footer-finish-top">
+                        <div className="footer-finish-top-left">
+                            <img src={ImgLogo} className="footer-logo" />
+                        </div>
+                        <div className="footer-finish-top-right">
+                            <a href="https://es.casino.guru/juega-en-linea-casino-resena" target="_blank">
+                                <img src={ImgJelguru} alt="Jelguru" className="svg" />
+                            </a>
+                            <a href="https://cert.gcb.cw/certificate?id=ZXlKcGRpSTZJbkpXWjJNeUx6QmxNbTVrU1dZeGVrMVZVbmQxY0hjOVBTSXNJblpoYkhWbElqb2laamR2V1Vwak5scGplREkwTjJZd1ZFRktUa0ozWjBsc1VqVmtXVmRzZGpKSFIzQnBUMHRVU1RGSGF6MGlMQ0p0WVdNaU9pSTJNVFprTnpWbFl6YzRORE5qWVdVNU1qTXdaVEEzTjJWalpqTTNaR0ZtTkRFd1ltWTJOR1prTUdVMU56ZzFZelF6TlRNeE1HRTRPV1JoTlRreU5HSTVJaXdpZEdGbklqb2lJbjA9" target="_blank">
+                                <img src={ImgGcbgreen} alt="" />
+                            </a>
+                            <img src={Img18} alt="" className="contrast-none" />
+                        </div>
+                    </div>
+                    <div className="footer-finish-body">
+                        <div className="footer-finish-body-left">
+                            <br />
+                            <p>
+                                <p>Juego solo para mayores de 18 años.</p>
+                                <p>Los juegos y apuestas deportivas a distancia realizadas en exceso pueden causar ludopatía.</p>
+                            </p>
+                        </div>
+                        <div className="footer-finish-body-left">Para registrarse en este sitio web, el usuario debe aceptar los. En caso de que los Términos y Condiciones Generales sean actualizados, los usuarios existentes pueden optar por dejar de utilizar los productos y servicios antes de que dicha actualización entre en vigor, lo cual es un mínimo de dos semanas después de haber sido anunciada.</div>
+                        <div className="footer-finish-body-right">
+                            <p>
+                                <p>Juegaenlinea.com is operated by Games &amp; More B.V. registered under No. 149948 at, Hanchi Snoa 19, Trias Building, Curaçao. This website is licensed and regulated by GCB (Curacao Gaming Control Board) under the license No. OGL/2023/121/0086.</p>
+                            </p>
                         </div>
                     </div>
                 </div>
